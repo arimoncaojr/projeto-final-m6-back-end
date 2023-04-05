fea22a36ce16cb9d1e03e64bfe27b2394cb471ff
arimoncaojr <106547012+arimoncaojr@users.noreply.github.com>
2023-04-05 10:45:54 -0300

    first commit

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import "express-async-errors";
+import handleError from "./errors/handleError";
+
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(handleError);
+
+export default app;
