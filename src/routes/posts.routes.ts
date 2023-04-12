f71bf36ce0e127d03638c8c2399b8412c8f1a026
arimoncaojr <106547012+arimoncaojr@users.noreply.github.com>
2023-04-12 12:30:38 -0300

    feat: criação de comentários

--- src/routes/posts.routes.ts
+++ src/routes/posts.routes.ts
@@ -2,7 +2,9 @@ import { Router } from "express";
 import { ensureAuthMiddleware } from "../middlewares/ensureAuth.middleware";
 import { ensureDataIsValidMiddleware } from "../middlewares/ensureDataIsValid.middleware";
 import { postSerializer } from "../serializers/post.serializers";
+import { commentRequestSerializer } from "../serializers/comment.serializers";
 import { createPostController } from "../controllers/posts.controller";
+import { createCommentController } from "../controllers/comments.controller";
 
 export const postsRoutes = Router();
 
@@ -11,4 +13,11 @@ postsRoutes.post(
   ensureAuthMiddleware,
   ensureDataIsValidMiddleware(postSerializer),
   createPostController
+);
+
+postsRoutes.post(
+  "/:id/comments",
+  ensureAuthMiddleware,
+  ensureDataIsValidMiddleware(commentRequestSerializer),
+  createCommentController
 );
