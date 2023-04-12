393614bafc1484f44e6a43abff5d078ef82de964
Thiago Rodrigues <thiago.rds@outlook.com.br>
2023-04-12 18:43:49 -0400

    fix: corrigido um merge

--- src/controllers/posts.controller.ts
+++ src/controllers/posts.controller.ts
@@ -6,14 +6,12 @@ import { delistPostService } from "../services/posts/delistPost.service";
 import { listPostsService } from "../services/posts/listPosts.service";
 import { listPostByIdService } from "../services/posts/listPostById.service";
 
-
 export const createPostController = async (req: Request, res: Response) => {
   const reqData: IPostRequest = req.body;
   const user = req.user;
   const data = await createPostService(reqData, user);
   return res.status(201).json(data);
 };
-
 
 export const deletePostController = async (req: Request, res: Response) => {
   const idPost = req.params.id
@@ -38,4 +36,3 @@ export const listPostByIdController = async (req: Request, res: Response) => {
   const ads = await listPostByIdService(req.params.id);
   return res.status(200).json(ads);
 };
-
