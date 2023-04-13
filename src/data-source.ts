13d286a31b81e8b059f3bd202e9f80a40e1ba0eb
helbertabreu <helbertfotografia@gmail.com>
2023-04-13 10:04:43 -0300

    fix: listagem de um anuncio retorna os dados corretos do usuario, list de todos os anuncios nao retorna anuncio com isActive false

--- src/data-source.ts
+++ src/data-source.ts
@@ -6,8 +6,7 @@ import { Address } from "./entities/address.entity";
 import { Post } from "./entities/post.entity";
 import { Image } from "./entities/image.entity";
 import { Comment } from "./entities/comment.entity";
-import { CreateEntities1681240167913 as CreateEntities } from "./migrations/1681240167913-createEntities";
-import { AlterTable1681315760952 as AlterTable } from "./migrations/1681315760952-alterTable";
+import { CreateEntity1681388425915 } from "./migrations/1681388425915-createEntity";
 
 const AppDataSource = new DataSource(
   process.env.NODE_ENV === "production"
@@ -15,7 +14,7 @@ const AppDataSource = new DataSource(
         type: "postgres",
         url: process.env.DATABASE_URL,
         entities: [User, Address, Post, Image, Comment],
-        migrations: [CreateEntities,AlterTable],
+        migrations: [CreateEntity1681388425915],
       }
     : {
         type: "postgres",
@@ -27,7 +26,7 @@ const AppDataSource = new DataSource(
         logging: true,
         synchronize: false,
         entities: [User, Address, Post, Image, Comment],
-        migrations: [CreateEntities,AlterTable],
+        migrations: [CreateEntity1681388425915],
       }
 );
 
